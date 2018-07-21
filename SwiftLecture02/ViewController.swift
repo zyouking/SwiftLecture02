e169a02cecc8b6fe2638c3d23ff3e1b236cb2e02
zyouking <zyouqing@qq.com>
2018-07-21 17:33:03 +0800

    init

---
+++ SwiftLecture02/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  SwiftLecture02
+//
+//  Created by stu1 on 2018/7/21.
+//  Copyright © 2018年 stu1. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print("hello,swift")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
