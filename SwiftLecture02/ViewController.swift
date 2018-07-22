6045a18d749185dfa045306f0185fc90ad91786c
zyouking <zyouqing@qq.com>
2018-07-22 16:27:40 +0800

    Timer and break

--- SwiftLecture02/ViewController.swift
+++ SwiftLecture02/ViewController.swift
@@ -26,20 +26,30 @@ class ViewController: UIViewController {
         var floats: Array<Float>=Array(repeating: 0, count: 5)
         
         ints.sort()
-        for index in 0..<3{
-            print("\(ints[index])")
-        }
-        
-        for str in english{
-            print(str)
-        }
+//        for index in 0..<3{
+//            print("\(ints[index])")
+//        }
+//
+//        for str in english{
+//            print(str)
+//        }
         
         //dictionary
-        let 吧=[1:"A",2:"C",3:"B"]
-        for(a,b) in 吧{
+        let dic=[1:"A",2:"C",3:"B"]
+        for(a,b) in dic{
             print("\( a ):\(b)")
         }
         
+        let dicSorted=dic.sorted(by: {$0.key<$1.key})
+        for(a,b) in dicSorted{
+            print("\( a ):\(b)")
+        }
+        
+        var times:Int=0
+        Timer .scheduledTimer(withTimeInterval: 1.0, repeats: true, block: {smalltimer in
+            times=times+1
+            print("timer:\(smalltimer.isValid)")
+        })
     }
 
     override func didReceiveMemoryWarning() {
